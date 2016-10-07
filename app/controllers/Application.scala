b49076a3c75ed26b5cbde1f3383417268bbf1140
tsubasa_matsukawa <t_matsukawa@r.recruit.co.jp>
2016-10-07 17:38:46 +0900

    modified Application

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,22 @@
 package controllers
 
+import models.Users
+import org.joda.time.DateTime
 import play.api.mvc._
+import scalikejdbc.AutoSession
 
 class Application extends Controller {
 
+  implicit val session = AutoSession
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+
+    val user = Users.find(1) match {
+      case Some(x) => x
+      case _ => Users.create("test",DateTime.now, DateTime.now)
+    }
+    Ok(user.toString)
   }
+
 
 }
